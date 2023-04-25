906af47603c3a97026c872ac5acdc9d8d48c1ebc
Diana Tran <dt.dianatran@gmail.com>
2023-04-24 21:37:44 -0600

    Feature 4 complete

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from projects.models import *
+
 
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "owner",
+    )
